a4fb428f96ce12150b6e78eddcc9040ee773f1b1
Pratik Patel <pratik.patel4057@gmail.com>
2022-08-21 23:07:49 +0530

    login + logout functionality

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -9,8 +9,16 @@
         <li><a href="{{ route('home.index') }}" class="nav-link px-2 text-secondary">Home</a></li>
       </ul>
 
+      @auth
+        {{auth()->user()->name}}
+        <div class="text-end">
+          <a href="{{ route('logout.perform') }}" class="btn btn-outline-light me-2">Logout</a>
+        </div>
+      @endauth
+
       @guest
         <div class="text-end">
+          <a href="{{ route('login.perform') }}" class="btn btn-outline-light me-2">Login</a>
           <a href="{{ route('register.perform') }}" class="btn btn-warning">Sign-up</a>
         </div>
       @endguest
--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,17 @@ Route::group(['namespace' => 'App\Http\Controllers'], function()
          */
         Route::get('/register', 'RegisterController@show')->name('register.show');
         Route::post('/register', 'RegisterController@register')->name('register.perform');
+        /**
+         * Login Routes
+         */
+        Route::get('/login', 'LoginController@show')->name('login.show');
+        Route::post('/login', 'LoginController@login')->name('login.perform');
+    });
 
+    Route::group(['middleware' => ['auth']], function() {
+        /**
+         * Logout Routes
+         */
+        Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
     });
 });
